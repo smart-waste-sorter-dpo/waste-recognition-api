6363512aa909ee5808628cf03227cf5a4ec3b692
Илья <128465818+Karasik876@users.noreply.github.com>
2025-02-26 14:50:31 +0500

    Update wastes.py

--- src/app/api/v1/routers/wastes.py
+++ src/app/api/v1/routers/wastes.py
@@ -36,7 +36,7 @@ async def classify_waste(file: UploadFile = File(...)) -> JSONResponse:
     if file.content_type != "image/jpeg":
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Invalid file type. Please upload an JPG file.",
+            detail="Invalid file type. Please upload a JPG file.",
         )
 
     image_bytes = await file.read()
